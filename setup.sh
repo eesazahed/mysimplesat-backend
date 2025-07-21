80b9dc1cb40f13859027a1a2b7c563ee9342ddf4
Eesa Zahed <eesa.zahed@gmail.com>
2025-07-20 22:23:54 -0500

    edit setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+echo "Starting process..."
+
 if [ "$#" -ne 1 ]; then
   echo "Parameter required PORT"
   exit 1
@@ -10,10 +12,14 @@ if ! [[ "$PORT" =~ ^[0-9]+$ ]]; then
   exit 1
 fi
 
-kill -9 $(lsof -ti ":$PORT") 2>/dev/null
+PIDS=$(timeout 2s lsof -ti ":$PORT")
+if [ -n "$PIDS" ]; then
+  kill -9 $PIDS
+fi
+
 git pull
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-gunicorn -b ":$PORT" app:app
 
+gunicorn -b ":$PORT" app:app --access-logfile - --error-logfile -
